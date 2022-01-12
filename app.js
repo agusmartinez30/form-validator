89bb4f43522d9eaf354bbd702250b312eea37630
unknown <agusmartinezy30@gmail.com>
2022-01-12 20:24:41 -0300

    clean code y validaciones

--- app.js
+++ app.js
@@ -16,36 +16,22 @@ const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z
     alertSuccess.textContent = "Mensaje enviado con éxito";
 };
 
-// const pintarMensajeError = (errores) => {
-//     errores.forEach((item) => {
-//         // item.tipo.classList.remove("error");
-//         item.tipo.textContent = item.msg;
-//     });
-// };
-
 const mostrarMsgError = (errores) => {
     errores.forEach( item => {
         alertSuccess.classList.add("ocultar");
         item.tipo.classList.add('error')
         item.tipo.textContent = item.msg
     });
-
-    console.log(errores);
 }
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    
-
-    // alertSuccess.classList.add("error");
-
     const errores = [];
 
     // validar nombre
      if (!regUserName.test(userName.value) || !userName.value.trim()){
-        console.log('user no valido');
-    //    alertName.textContent = "Nombre de usuario no valido"
+        // console.log('user no valido');
         alertName.classList.remove('ocultar')
 
        errores.push({
@@ -58,36 +44,11 @@ formulario.addEventListener("submit", (e) => {
          console.log('user valido');
      }
 
-    //      alertName.classList.add("error");
-    //    
-    //      errores.push({
-    //          tipo: alertName,
-    //          msg: "Formato no válido campo nombre, solo letras",
-    //      });
-    // } else {
-    //      userName.classList.remove("error");
-    //      userName.classList.add("exito");
-    //     alertName.classList.add("exito");
-    // }
 
-    // validar email
-    // if (!regUserEmail.test(userEmail.value) || !userEmail.value.trim()) {
-    //     userEmail.classList.add("error");
-
-    //     errores.push({
-    //         tipo: alertEmail,
-    //         msg: "Escriba un correo válido",
-    //     });
-    // } else {
-    //     userEmail.classList.remove("error");
-    //     userEmail.classList.add("exito");
-    //     alertEmail.classList.add("error");
-    // }
-
+    //VALIDAR EMAIL
 
     if (!regUserEmail.test(userEmail.value) || !userEmail.value.trim()){
-        console.log('user no valido');
-    //    alertName.textContent = "Nombre de usuario no valido"
+        // console.log('user no valido');
         alertEmail.classList.remove('ocultar')
 
        errores.push({
